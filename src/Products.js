3b2cee3886ba0c949f7eba2d4976d1610d8a75af
Alireza Ameri <a.ameri1379@yahoo.com>
2021-11-25 00:44:16 +0330

    add discription

--- src/Products.js
+++ src/Products.js
@@ -3,14 +3,14 @@ const products = [
     id: "1",
     name: "iphone 8",
     image: "/image/iphone 8.jpg",
-    discription: "توضیحات محصول",
+    discription: " Released 2017, September 22",
     price: 15,
   },
   {
     id: "2",
     name: "iphone x",
     image: "/image/iphone x.jpg",
-    discription: "توضیحات محصول",
+    discription: "Also known as Apple iPhone 10, Apple iPhone Ten",
     price: 20,
   },
 
@@ -18,14 +18,14 @@ const products = [
     id: "3",
     name: "iphone 12",
     image: "/image/iphone-12.png",
-    discription: "توضیحات محصول",
+    discription: "Released 2020, October 23",
     price: 15,
   },
   {
     id: "4",
     name: "iphone 11 pro",
     image: "/image/iphone.jpg",
-    discription: "توضیحات محصول",
+    discription: "Released 2019, September 20",
     price: 15,
   },
 ];

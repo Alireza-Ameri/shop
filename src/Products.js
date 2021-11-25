41e19627ff206c81c62e4af52dafa0577304f697
Alireza Ameri <a.ameri1379@yahoo.com>
2021-11-25 23:21:09 +0330

    add price

--- src/Products.js
+++ src/Products.js
@@ -4,14 +4,14 @@ const products = [
     name: "iphone 8",
     image: "/image/iphone 8.jpg",
     discription: " Released 2017, September 22",
-    price: 15,
+    price: 800,
   },
   {
     id: "2",
     name: "iphone x",
     image: "/image/iphone x.jpg",
     discription: "Also known as Apple iPhone 10, Apple iPhone Ten",
-    price: 20,
+    price: 900,
   },
 
   {
@@ -19,14 +19,14 @@ const products = [
     name: "iphone 12",
     image: "/image/iphone-12.png",
     discription: "Released 2020, October 23",
-    price: 15,
+    price: 1100,
   },
   {
     id: "4",
     name: "iphone 11 pro",
     image: "/image/iphone.jpg",
     discription: "Released 2019, September 20",
-    price: 15,
+    price: 1000,
   },
 ];
 
